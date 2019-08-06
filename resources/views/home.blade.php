67d45b5c3d800fcc3550154371e690de1e43fe20
Kevin Namuag <kevsnams@pm.me>
2019-08-06 16:40:57 +0800

    remove pageHeader on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,5 @@
 @extends('layouts.app')
 
-@section('pageHeader')
-    <span class="uk-text-large">Welcome { ADMIN_NAME }</span>
-@endsection
-
 @section('content')
     <div style="height: 800px" class="uk-text-center">
         <img src="<?php echo url('/splash.png') ?>">
